ba950b334e07b95382b84b02ec78e20884f085f9
pajid9763 <pajid9763@wrdsb.ca>
2020-10-08 13:08:57 +0000

    finished program that creates search algorithm

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * program that creates search algorithm
+ * @author Darian 
  */
 public class Main {
 
@@ -9,7 +10,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+
+    int numbers[] = new int[10];
+
+    // ask the user to enter 10 integers
+    System.out.println("Please enter in 10 integers to put into the array");
+    for (int i = 0; i < numbers.length; i++){
+      numbers[i] = input.nextInt();
+    }
+
+    // ask the user what number they want to find
+    System.out.println("Please enter a number to find");
+    int numFind = input.nextInt();
+
+    // locate the desired number
+    for (int i = 0; i < numbers.length; i++){
+      if (numbers[i] == numFind){
+        System.out.println(numFind + " is located at index " + i);
+
+      }else if (i < numFind || i > numFind){
+
+      }
+      
+    }
     
   }
 }
